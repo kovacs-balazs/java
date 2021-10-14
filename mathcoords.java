8b34d2a2f1ab1990a13cd0d2b978ff514a0babe1
kovacs-balazs <56174644+kovacs-balazs@users.noreply.github.com>
2021-10-14 17:06:10 +0200

    Update mathcoords.java

--- mathcoords.java
+++ mathcoords.java
@@ -5,9 +5,9 @@ int maxX = Math.max(-201, -200);
 int maxY = Math.max(61, 63);
 int maxZ = Math.max(-15, -17);
 
-for(int x = minX; x < maxX; x++) {
-    for (int y = minY; y < maxY; y++) {
-        for (int z = minZ; z < maxZ; z++) {
+for(int x = minX; x <= maxX; x++) {
+    for (int y = minY; y <= maxY; y++) {
+        for (int z = minZ; z <= maxZ; z++) {
             Location loc = new Location(world, x, y, z);
             loc.getBlock().setType(Material.AIR);
         }
